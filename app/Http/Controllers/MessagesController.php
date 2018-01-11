18276af04efe44e4decb95fc8a8468b6209057ef
Darwel Quintero <darwel34@gmail.com>
2018-01-11 08:39:00 -0430

    se creo el controlador para la clase Message

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Message;
+
+class MessagesController extends Controller
+{
+    // public function show($id){
+    //     $message = Message::find($id);
+
+    //     return view('messages.show', [
+    //         'message' => $message
+    //     ]);
+    // }
+    public function show(Message $message){
+
+        return view('messages.show', [
+            'message' => $message
+        ]);
+    }
+}
