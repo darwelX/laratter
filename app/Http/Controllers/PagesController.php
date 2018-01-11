1d8edf965dc289eee34271f5a9984f64e054eaee
Darwel Quintero <darwel34@gmail.com>
2018-01-11 08:14:45 -0430

    se esta usando el orm eloquent

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,32 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class PagesController extends Controller
 {
     public function home(){
-        $messages = [
-            [ 
-                'id' => 1,
-                'content' => 'Este es mi primer mensaje',
-                'image' => 'https://lorempixel.com/600/300?1'
-            ],
-            [ 
-                'id' => 2,
-                'content' => 'Este es mi segundo mensaje',
-                'image' => 'https://lorempixel.com/600/300?2'
-            ],
-            [ 
-                'id' => 3,
-                'content' => 'Este otro mensaje',
-                'image' => 'https://lorempixel.com/600/300?3'
-            ],
-            [ 
-                'id' => 4,
-                'content' => 'Este es mi último mensaje',
-                'image' => 'https://lorempixel.com/600/300?4'
-            ]
-        ];
+        $messages = Message::all();
+
+        // dd($messages);
         return view('welcome', [
             'messages' => $messages,
         ]);
