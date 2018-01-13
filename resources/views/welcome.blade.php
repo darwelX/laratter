11e577c33fd47e2bf84c89bac0424309ecc06c13
Darwel Quintero <darwel34@gmail.com>
2018-01-13 11:13:47 -0430

    se agrego el formulario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,41 @@
   </div>
 
   <div class="row">
+    <div class="col-12 col-sm-6 offset-sm-3 col-md-6 offset-md-3">
+        <div class="card panel">
+            <div class="card-block">
+                <div class="card-header">
+                    <div class="card-title">Nuevo</div>
+                </div>
+                <form action="/messages/create" method="post"> 
+                    {{csrf_field()}}
+                    <div class="form-group">
+                        <label for="contenido">
+                            Contenido
+                        </label>
+                        @if($errors->has('contenido'))
+                            @foreach($errors->get('contenido') as $error)
+                                <div class="alert alert-danger alert-dismissible fade show">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                    {{$error}}
+                                </div>
+                            @endforeach
+                        @endif
+                        <textarea name="contenido" id="contenido" class="form-control @if($errors->has('contenido')) is-invalid @endif" cols="30" rows="5" placeholder="Que estas pensando?"></textarea>
+                        <!-- <div class="invalid-feedback">Error</div> -->
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" value="Enviar" class="btn btn-primary">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+  </div>
+  <div class="row">
     @forelse($messages as $message)
         <div class="col-6">
             <img class="img-thumbnail" src="{{$message->image}}" alt="">
