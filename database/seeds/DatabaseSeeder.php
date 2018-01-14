600e0f61b9b81d5dec0b18783a480dd8ccb3229e
Darwel Quintero <darwel34@gmail.com>
2018-01-14 10:17:28 -0430

    se creo una semilla de 100 instancias de la clase Message

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,11 +6,18 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
+     * Para ejecutar esto: php artisan db:seed
+     * Para borrar toda la BD crearla a partir de las migrations y seed: php artisan migrate:refresh --seed
      * @return void
      */
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // crea 100 instancias de mensajes
+        // factory(App\Message::class, 100)->create();
+        // crea 100 registros en la base de datos de la clase Message
+        factory(App\Message::class)
+            ->times(100)
+            ->create();
     }
 }
