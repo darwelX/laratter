455c3ea39a8dc3150a788ae4252078a4a0ae23a9
Darwel Quintero <darwel34@gmail.com>
2018-01-14 10:16:48 -0430

    se creo la fatoria

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Message::class, function (Faker $faker){
+    return [
+        // retorna palabras al azar en formato texto (lorem ipsum)
+        // 'content' => $faker->works(5, true)
+        // retorna palabras en array (lorem ipsum)
+        // 'content' => $faker->works(5)
+        // retorna una sola palabra (lorem ipsum)
+        // 'content' => $faker->work
+        // retorna parrafos (lorem ipsum)
+        // 'content' => $faker->paragrah
+        // retorna texto basado en un libro (alicia en el pais de las maravillas)
+        // 'content' => $faker->realText()
+        'content' => $faker->realText(random_int(20, 160)),
+        'image'   => $faker->imageUrl(600, 300)
+    ];
+});
