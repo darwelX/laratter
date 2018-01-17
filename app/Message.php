708436c24be2b2a8c658f3a13f6687f2444ed302
Darwel Quintero <darwel34@gmail.com>
2018-01-17 14:49:37 -0430

    se agrego la relacion beleongsTo que tiene la clase con el mensaje

--- app/Message.php
+++ app/Message.php
@@ -8,4 +8,8 @@ class Message extends Model
 {
     // esta propiedad evita protejer columnas, para asi evitar el error MassAssignmentException
     protected $guarded = [];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
