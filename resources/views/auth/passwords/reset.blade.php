b3c7e346988cee95a408bf1dcb290adb7c884c31
Darwel Quintero <darwel34@gmail.com>
2018-01-17 08:46:39 -0430

    se maqueto el formulario

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -16,9 +16,9 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-                            <label for="email" class="col-md-4 form-control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 offset-md-3 form-control-label">E-Mail Address</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 offset-md-3">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -30,9 +30,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
-                            <label for="password" class="col-md-4 form-control-label">Password</label>
+                            <label for="password" class="col-md-4 offset-md-3 form-control-label">Password</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 offset-md-3">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -44,8 +44,8 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-danger' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
+                            <label for="password-confirm" class="col-md-4 offset-md-3 form-control-label">Confirm Password</label>
+                            <div class="col-md-6 offset-md-3">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
 
                                 @if ($errors->has('password_confirmation'))
@@ -57,7 +57,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     Reset Password
                                 </button>
