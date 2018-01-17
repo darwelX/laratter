a2f2529fe4281bf7a10a2778229e6a7cdf3376c6
Darwel Quintero <darwel34@gmail.com>
2018-01-17 15:57:08 -0430

    se agrego comentario a la relacion

--- app/Message.php
+++ app/Message.php
@@ -9,6 +9,10 @@ class Message extends Model
     // esta propiedad evita protejer columnas, para asi evitar el error MassAssignmentException
     protected $guarded = [];
 
+    /**
+     * Relacion de pertenencia.
+     * Un mensaje pertenece a un usuario
+     */
     public function user(){
         return $this->belongsTo(User::class);
     }
