21b9bac351ceb0188a2d6ed381f0fa2c66930385
Darwel Quintero <darwel34@gmail.com>
2018-01-17 14:59:14 -0430

    se creo la plantilla reutilizable para mostrar un mensaje

---
+++ resources/views/messages/message.blade.php
@@ -0,0 +1,5 @@
+<img class="img-thumbnail" src="{{$message->image}}" alt="">
+    <div class="text-muted">Escrito por: <a href="/{{$message->user->username}}">{{$message->user->name}}</a></div>
+    <p class="card-text">{{$message->content}}
+    <a href="/messages/{{$message->id}}">Leer más..</a>
+</p>
