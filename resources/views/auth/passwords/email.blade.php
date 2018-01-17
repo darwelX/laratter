399134220f2156351848f7fa122bfee2c5c4ccc2
Darwel Quintero <darwel34@gmail.com>
2018-01-17 08:47:12 -0430

    se maqueto el formulario

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,28 +3,30 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+        <div class="col-md-8 offset-md-2 mt-3">
+            <div class="panel card">
+                <div class="card-block">
+                    <div class="card-header">
+                        <div class="panel-title">Recuperar Password</div>
+                    </div>
 
-                <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
+                    <form class="mt-3" method="POST" action="{{ route('password.email') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                            <label for="email" class="col-md-4 offset-md-3 form-control-label">Dirección de E-Mail</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7 offset-md-3">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
+                                    <span class="form-control-feedback">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
@@ -32,9 +34,9 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-6 offset-md-3">
                                 <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
+                                    Envie el Link para Resetear el Password
                                 </button>
                             </div>
                         </div>
