f785933a9e62c15c2771ed8c7e7f1e2dcb1eb465
Darwel Quintero <darwel34@gmail.com>
2018-01-17 14:57:21 -0430

    se modifico el factory para que cree 50 usuarios y por cada usuario cree 20 mensajes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,8 +16,12 @@ class DatabaseSeeder extends Seeder
         // crea 100 instancias de mensajes
         // factory(App\Message::class, 100)->create();
         // crea 100 registros en la base de datos de la clase Message
-        factory(App\Message::class)
-            ->times(100)
-            ->create();
+        factory(App\User::class, 50)->create()->each(function(App\User $user){
+            factory(App\Message::class)
+                ->times(20)
+                ->create([
+                    'user_id' => $user->id,
+                ]);
+        });
     }
 }
