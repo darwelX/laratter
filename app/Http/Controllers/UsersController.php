6882f57374188cb60ae4cddbaad59ac51edc90ce
Darwel Quintero <darwel34@gmail.com>
2018-01-18 07:31:38 -0430

    se refactorizo y se creo dos nuevas acciones

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,12 +11,38 @@ class UsersController extends Controller
 {
     public function show($username){
         // se esta utilizando query builder
-        $user = User::where('username', $username)->first();
+        $user = $this->findByUsername($username);
         $messages = Message::where('user_id', $user->id)->paginate(10);
         // dd($mes)
         return view('users.show', [
             'user' => $user,
             'messages' => $messages,
         ]);
+    }
+
+    public function follows($username)
+    {
+        $user = $this->findByUsername($username);
+        // dd($user);
+        return view('users.follows', [
+            'user' => $user
+        ]);
+    }
+
+    
+    public function follow($username, Request $request)
+    {
+        $user = $this->findByUsername($username);
+        
+        $yo = $request->user();
+        
+        $yo->follows()->attach($user);
+        
+        return redirect("/$username")->withSuccess('Usuario seguido!');
+    }
+
+    private function findByUsername($username)
+    {
+        return User::where('username', $username)->first();
     }
 }
