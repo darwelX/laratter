62ddcd3e2a6e22d170d52c9193ef6e1929398142
Darwel Quintero <darwel34@gmail.com>
2018-01-18 07:33:53 -0430

    se creo el boton de follow

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,6 +3,13 @@
 @section('content')
 
 <h1>{{$user->name}}</h1>
+<form action="/{{$user->username}}/follow" method="post" class="mb-2">
+  {{ csrf_field() }}
+  @if(session('success'))
+    <span class="text-success">{{ session('success') }}</span>
+  @endif
+  <button class="btn btn-primary">Follow</button>
+</form>
   <div class="row">
 @forelse($messages as $message)
     <div class="col-6">
