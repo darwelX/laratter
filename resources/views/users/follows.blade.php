82dc9a85cda815d41c1d60e22cb686ebe3d7e264
Darwel Quintero <darwel34@gmail.com>
2018-01-18 14:14:48 -0430

    se maqueto el texto

--- resources/views/users/follows.blade.php
+++ resources/views/users/follows.blade.php
@@ -6,7 +6,7 @@
             <div class="card mt-2 p-1" style="width: 20rem;">
                 <img class="card-img-top rounded-circle img-fluid p-3" src="{{$user->avatar}}" alt="{{$user->name}}">
                 <div class="card-block">
-                    <h4 class="card-title"><a href="/{{$user->username}}" class="btn btn-link">{{$user->username}}</a></h4>
+                <a href="/{{$user->username}}" class="btn btn-link"><h4 class="card-title">{{$user->username}}</h4></a>
                 </div>
                 <ul class="list-group list-group-flush">
                     @foreach($follows as $follow)
