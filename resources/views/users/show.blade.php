3610330b8c756270889c6e145fd738137ee41aed
Darwel Quintero <darwel34@gmail.com>
2018-01-18 09:13:36 -0430

    se agregaron los enlaces para ver a quien sigo y quien me sigue ademas de ver el boton dejar de seguir

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,13 +3,31 @@
 @section('content')
 
 <h1>{{$user->name}}</h1>
-<form action="/{{$user->username}}/follow" method="post" class="mb-2">
-  {{ csrf_field() }}
-  @if(session('success'))
-    <span class="text-success">{{ session('success') }}</span>
+<a href="/{{$user->username}}/follows">
+  Sigue a <span class="badge badge-pill badge-success">{{ ($user->follows->count()>0)? $user->follows->count() : 0 }}</span>
+</a>
+<a href="/{{$user->username}}/followed">
+  Seguido por <span class="badge badge-pill badge-info">{{ $user->follows->count()}}</span>
+</a>
+  @if(Auth::check())
+    @if(Auth::user()->isFollowing($user))
+      <form action="/{{$user->username}}/unfollow" method="post" class="mb-2 mt-2">
+        {{ csrf_field() }}
+        @if(session('success'))
+          <span class="text-success">{{ session('success') }}</span>
+        @endif
+        <button class="btn btn-danger">Dejar de Seguir</button>
+      </form>
+    @else
+      <form action="/{{$user->username}}/follow" method="post" class="mb-2 mt-2">
+        {{ csrf_field() }}
+        @if(session('success'))
+          <span class="text-success">{{ session('success') }}</span>
+        @endif
+        <button class="btn btn-primary">Seguir</button>
+      </form>    
+    @endif
   @endif
-  <button class="btn btn-primary">Follow</button>
-</form>
   <div class="row">
 @forelse($messages as $message)
     <div class="col-6">
