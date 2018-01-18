94eb8b3f3bd883f088e9fa1dc61d2b0f9d4baca7
Darwel Quintero <darwel34@gmail.com>
2018-01-18 07:33:03 -0430

    se reconstruyo para que cada usuario siga a 10 usuarios al azar

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -7,6 +7,7 @@ class DatabaseSeeder extends Seeder
     /**
      * Run the database seeds.
      * Para ejecutar esto: php artisan db:seed
+     * Para ejecutar desde cero: php artisan migrate:refresh --seed
      * Para borrar toda la BD crearla a partir de las migrations y seed: php artisan migrate:refresh --seed
      * @return void
      */
@@ -16,12 +17,16 @@ class DatabaseSeeder extends Seeder
         // crea 100 instancias de mensajes
         // factory(App\Message::class, 100)->create();
         // crea 100 registros en la base de datos de la clase Message
-        factory(App\User::class, 50)->create()->each(function(App\User $user){
+        $users = factory(App\User::class, 50)->create();
+        $users->each(function(App\User $user) use ($users) {
             factory(App\Message::class)
                 ->times(20)
                 ->create([
                     'user_id' => $user->id,
                 ]);
+            $user->follows()->sync(
+                $users->random(10)
+            );
         });
     }
 }
