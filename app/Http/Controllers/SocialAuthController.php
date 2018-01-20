5bd75f67edbe18777e0c782eb5328e0867291988
Darwel Quintero <darwel34@gmail.com>
2018-01-20 07:00:04 -0430

    se creo la logica para registrar un usuario con facebook

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Socialite;
+use App\User;
+use App\SocialProfile;
 
 class SocialAuthController extends Controller
 {
@@ -15,6 +17,36 @@ class SocialAuthController extends Controller
     public function callback()
     {
         $user = Socialite::driver('facebook')->user();
-        dd($user);
+        session()->flash('facebookUser', $user);
+        // dd(session('facebookUser'));
+
+        return view('users.facebook', [
+            'user' => $user,
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $data = session('facebookUser');
+
+        // dd($data);
+        $username = $request->input('username');
+
+        $user = User::create([
+            'name' => $data->name,
+            'email' => $data->email,
+            'avatar' => $data->avatar,
+            'username' => $username,
+            'password' => str_random(16),
+        ]);
+
+        $profile = SocialProfile::create([
+            'social_id' => $data->id,
+            'user_id' => $user->id,
+        ]);
+
+        auth()->login($user);
+
+        return redirect('/');
     }
 }
