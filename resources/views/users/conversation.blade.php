e1f78f2cc1acd8d4ab1d124336c0c70fa8145775
Darwel Quintero <darwel34@gmail.com>
2018-01-23 09:17:44 -0430

    se creo la plantilla de conversaciones

---
+++ resources/views/users/conversation.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+  <!-- el metodo except excluye un usuario por id -->
+  <!-- el metodo convierte un objeto a string pasandole como parametro el campo y el separador, porque pueden existir conversacion con multiples usuarios-->
+  <h3>Conversación con: <small class="text-muted">{{ $conversation->users->except($user->id)->implode('name', ', ') }}</small></h3>
+  <div class="card">
+    @foreach($conversation->privateMessages as $message)
+      <div class="card-block">
+        <blockquote class="blockquote">
+          <p><strong>{{ $message->user->name }}</strong> dijo...</p>
+          <p class="ml-3 font-italic">{{ $message->message }}</p>
+          <small class="text-muted">{{ $message->created_at}}</small>
+        </blockquote>
+      </div>
+    @endforeach
+  </div>
+@endsection
