bdef06f950a332f57e91943dc2a68ccabdcf8265
Darwel Quintero <darwel34@gmail.com>
2018-01-26 15:22:09 -0430

    se cambio la query para optización

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -50,7 +50,8 @@ class MessagesController extends Controller
     public function search(Request $request)
     {
         $query = $request->input('query');
-        $messages = Message::where('content', 'LIKE', "%$query%")->get();
+        // con with mejora el rendimeinto de la consulta ya que hace un solo query para traer todos los usuarios
+        $messages = Message::with('user')->where('content', 'LIKE', "%$query%")->get();
 
         return view('messages.index', [
             'messages' => $messages,
