1195ab0fa9c356eadd91eefea43ad3f7c05a3407
Darwel Quintero <darwel34@gmail.com>
2018-01-26 09:46:40 -0430

    se creo la plantilla de mostrar los resultados de la busqueda de mensajes

---
+++ resources/views/messages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="row">
+  @foreach($messages as $message)
+    <div class="col-6">
+      @include('messages.message')
+    </div>
+  @endforeach
+  </div>
+@endsection
