5a999f46d143a318973481e8f7d70a313895ffa0
Darwel Quintero <darwel34@gmail.com>
2018-01-26 09:18:16 -0430

    se crearon las plantillas personalizadas para los errores 404 y 500

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::layout')
+
+@section('title', 'Error - 500')
+
+@section('message', 'Sorry, we are having a temporary problem. We have been alerted and will be rolling out a fix soon')
