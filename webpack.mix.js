ad61bdf1a793af807385fe50c7871035a333606d
Darwel Quintero <darwel34@gmail.com>
2018-01-31 18:53:54 -0430

    haciendo pruebas

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,13 +13,16 @@ let mix = require('laravel-mix');
 
 mix.js([
   'node_modules/jquery/dist/jquery.min.js',
+  'node_modules/vue/dist/vue.min.js',
   'node_modules/tether/dist/js/tether.min.js',
-  'node_modules/bootstrap/dist/js/bootstrap.js',  
-  'resources/assets/js/app.js'
+  'node_modules/bootstrap/dist/js/bootstrap.js', 
+  'resources/assets/js/prueba.js',
+  'resources/assets/js/app.js',
 ], 'public/js/app.js')
 .sass('resources/assets/sass/app.scss', 'public/css/app.css');
 
 mix.autoload({
+  vue: ['vue', 'window.Vue'],
   jquery: ['$', 'jQuery', 'window.jQuery'],
   tether: ['Tether', 'window.Tether']
 });
