539e08c4dd41bb549b7c15b55a15a8a38d6e9c20
Darwel Quintero <darwel34@gmail.com>
2018-01-31 18:52:03 -0430

    se cambio la configuracion del componente

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,7 +4,7 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
+console.log('listo');
 require('./bootstrap');
 
 window.Vue = require('vue');
@@ -15,7 +15,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('responses', require('./components/Responses.vue'));
 
 const app = new Vue({
     el: '#app'
