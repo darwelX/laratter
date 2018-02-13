9a2534e8de43daa0b19017173700c4b10620ed7d
Darwel Quintero <darwel34@gmail.com>
2018-02-12 20:43:17 -0430

    se modifico el estilo de mostrar el perfil del usuario y se oculto el boton de follow en caso de tratarse del mismo usuario

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,14 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="row mt-2">
+  <div class="col-2">
+    <img class="card-img-top rounded-circle img-fluid avatar-medium" src="{{$user->avatar}}" alt="{{$user->name}}">
+  </div>
+  <div class="col-8">
+    <h1 class="mt-5">{{$user->name}}</h1>
+    <private :username="'{{$user->username}}'"></private>
+  </div>
+</div>
+<div class="row mt-2">
+  <div class="col-12">
+    <a href="/{{$user->username}}/follows">
+      Sigue a <span class="badge badge-pill badge-success">{{ ($user->follows->count()>0)? $user->follows->count() : 0 }}</span>
+    </a>
+    <a href="/{{$user->username}}/followed">
+      Seguido por <span class="badge badge-pill badge-info">{{ $user->follows->count()}}</span>
+    </a>
+  </div>
+</div>
 
-<h1>{{$user->name}}</h1>
-<a href="/{{$user->username}}/follows">
-  Sigue a <span class="badge badge-pill badge-success">{{ ($user->follows->count()>0)? $user->follows->count() : 0 }}</span>
-</a>
-<a href="/{{$user->username}}/followed">
-  Seguido por <span class="badge badge-pill badge-info">{{ $user->follows->count()}}</span>
-</a>
   @if(Auth::check())
 
     @if(Gate::allows('dms', $user))
@@ -32,7 +44,7 @@
         @endif
         <button class="btn btn-danger">Dejar de Seguir</button>
       </form>
-    @else
+    @elseif(Auth::user()->username != $user->username)
       <form action="/{{$user->username}}/follow" method="post" class="mb-2 mt-2">
         {{ csrf_field() }}
         @if(session('success'))
@@ -41,8 +53,9 @@
         <button class="btn btn-primary">Seguir</button>
       </form>    
     @endif
-  @endif
-  <div class="row">
+  @endif <!-- fin de usuario autenticado -->
+
+<div class="row mt-3">
 @forelse($messages as $message)
     <div class="col-6">
       @include('messages.message')
@@ -55,6 +68,6 @@
         {{$messages->links('pagination::bootstrap-4')}}
       @endif
     </div>
-  </div>
+</div>
 
 @endsection
