874e9c0a08a73a3069ac5a3bc6649fd8451f99f8
Darwel Quintero <darwel34@gmail.com>
2018-02-13 16:13:44 -0430

    se añadio el username al titulo del mensaje

--- resources/views/messages/message.blade.php
+++ resources/views/messages/message.blade.php
@@ -1,7 +1,7 @@
 <div class="card mb-2">
   <img class="card-img-top" src="{{$message->image}}" alt="">
   <div class="card-block">
-    <h4 class="card-title">Mensaje</h4>
+    <h4 class="card-title">Mensaje de @<a href="/{{$message->user->username}}">{{$message->user->name}}</a></h4>
     <p class="card-text">{{$message->content}}</p>
     <a href="/messages/{{$message->id}}" class="btn btn-primary">Leer más..</a>
   </div>
