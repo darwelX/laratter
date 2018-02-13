a1f6f1e7a3a2a814da0ae11975db680e06a510dc
Darwel Quintero <darwel34@gmail.com>
2018-02-12 21:00:53 -0430

    se maqueto el espacio para mostrar el mensaje

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 @section('content')
-<h1 class="h3">Mensaje id: {{$message->id}}</h1>
-@include('messages.message')
-<responses :message="{{ $message->id}}"></responses>
+<div class="col-6 mx-auto mt-3">
+  <h1 class="h3">Mensaje id: {{$message->id}}</h1>
+  @include('messages.message')
+  <responses :message="{{ $message->id}}"></responses>
+</div>
 @endsection
