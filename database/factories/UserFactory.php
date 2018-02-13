6d01efffb513b720ef8c88001ac6575d76b5a787
Darwel Quintero <darwel34@gmail.com>
2018-02-12 20:35:31 -0430

    se agrego el status de cuenta publica por defecto

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,5 +23,6 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
         'username' => $faker->userName,
         'avatar' => $faker->imageUrl(300, 300, 'people'),
+        'private' => false,
     ];
 });
