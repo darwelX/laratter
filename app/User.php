d21a7562b926946fbed8056bda994f5510221626
Darwel Quintero <darwel34@gmail.com>
2018-02-18 20:55:13 -0430

    se agrego las relaciones de invitaciones ademas de los metodos que la evaluan

--- app/User.php
+++ app/User.php
@@ -60,4 +60,41 @@ class User extends Authenticatable
     {
         return $this->hasMany(\App\SocialProfile::class);
     }
+
+    public function invitations(){
+        return $this->belongsToMany(User::class, 'invitations', 'user_id' , 'user_for_id')->orderBy('created_at', 'desc')->withPivot('accepted');
+    }
+
+    public function invitationFrom()
+    {
+        return $this->belongsToMany(User::class, 'invitations', 'user_for_id' , 'user_id')->withPivot(['accepted','created_at', 'updated_at'])->orderBy('created_at', 'desc');
+    }
+
+    public function isInvitation(User $user){
+        $flag = false;
+        foreach($this->invitations as $invitation){
+            if($invitation->pivot->accepted == 0 && $invitation->id == $user->id){
+                $flag = true;
+                break;
+            }
+        }
+        return $flag;
+    }
+
+    public function isInvitationAccepted(USer $user)
+    {
+        $flag = false;
+        foreach($this->invitationFrom as $invitation)
+        {
+            if($invitation->pivot->accepted == 1 && $invitation->id == $user->id){
+                return true;
+            }
+        }
+        return $flag;
+    }
+
+    public function notInvitation(User $user)
+    {
+        return !$this->invitationFrom->contains($user);
+    }
 }
